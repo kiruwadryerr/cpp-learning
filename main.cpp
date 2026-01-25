6fd5a4f5ca773219d45e1c8161076e812bd45413
Kirill <dryer13123@gmail.com>
2026-01-25 20:12:58 +0500

    Add arithmetic examples and output

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 12;
+    int b = 4;
+
+    cout << "a + b = " << a + b << "\n";
+    cout << "a - b = " << a - b << "\n";
+    cout << "a * b = " << a * b << "\n";
+    cout << "a / b = " << a / b << "\n";
+    cout << "a % b = " << a % b << "\n";
+
+}
