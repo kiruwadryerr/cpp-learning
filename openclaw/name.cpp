cef18554746815db7ba6e9cb3a6220c78f3a5884
Kirill <dryer13123@gmail.com>
2026-02-24 16:15:08 +0500

    openclaw

---
+++ openclaw/name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "Как тебя зовут";
+    string name;
+    cin >> name;
+    cout << "Сколько лет?";
+    int age;
+    cin >> age;
+
+    cout << "Привет, " << name << "! Через 5 лет тебе будет " << age + 5 << " лет." << endl;
+}
