aacf96b4ebad62de6d2416a4d9dd6149af1cf183
Kirill <dryer13123@gmail.com>
2026-02-26 01:32:37 +0500

    skillbox tasks

---
+++ skillbox/6mod/prostoe6.3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Введите положительное число: ";
+    cin >> num;
+    int current = 1;
+    int count = 0;
+
+    while (current <= num)
+    {
+        if (num % current == 0)
+        {
+            count++;
+        }
+        current++;
+    }
+
+    if (count == 2)
+    {
+        cout << "Число простое." << endl;
+    } else {
+        cout << "Число не простое." << endl;
+    }
+}
